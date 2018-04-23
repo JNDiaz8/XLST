c1365134a3dd536c1df2267329088a59d299d127
Joaquin <joaquin.diaz.ramirez.8897@gmail.com>
2018-04-23 07:32:02 +0200

    Primer Commit

---
+++ src/main/java/com/iesvc/dam/lmsgi/xslt/xslt.java
@@ -0,0 +1,13 @@
+package com.iesvc.dam.lmsgi.xslt;
+
+/**
+ * Hello world!
+ *
+ */
+public class xslt 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
